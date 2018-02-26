d25cc90e0d05b10d4aeecdaa2abea2a4d28134af
TheRealLiviux <livio.rossani+github@gmail.com>
2018-02-26 13:07:24 +0100

    Add files via upload

---
+++ SerialRick.ino
@@ -0,0 +1,21 @@
+String head="Never gonna ";
+String verses[6]={"give you up","let you down","run around and desert you","make you cry","say goodbye","tell a lie and hurt you"};
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    ;
+  }
+}
+
+void loop() {
+  Serial.println("");
+  for(int i = 0 ; i < 6 ; i++) {
+    Serial.print(head);
+    Serial.println(verses[i]);
+    delay(500);
+  }
+  delay(2000);
+  Serial.println("");
+
+}
